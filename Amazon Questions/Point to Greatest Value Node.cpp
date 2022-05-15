607501d861b85d40c07ce6c93dd331c623c0e88d
Devang Chopra <65708528+devangchopra@users.noreply.github.com>
2022-05-15 15:52:18 +0530

    Add files via upload

---
+++ Amazon Questions/Point to Greatest Value Node.cpp
@@ -0,0 +1,48 @@
+/****************************************************************
+	Following is the Linked list node structure already written:
+
+	template <typename T>
+	class LinkedListNode {
+	public:
+		T data;
+		LinkedListNode* next;
+		LinkedListNode* arbit;
+
+		LinkedListNode(T data) {
+			this->next = NULL;
+			this-> arbit = NULL;
+			this->data = data;
+		}
+	};
+
+*****************************************************************/
+
+
+
+LinkedListNode<int>* reverse(LinkedListNode<int>*head)
+{
+	LinkedListNode<int>*prev = NULL, *current = head, *next;
+	while (current != NULL)
+	{
+		next  = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+	return prev;
+}
+LinkedListNode<int>* populateArbit(LinkedListNode<int>* head)
+{
+	head = reverse(head);
+	LinkedListNode<int> *max = head;
+	LinkedListNode<int> *temp = head->next;
+	while (temp != NULL)
+	{
+		temp->arbit = max;
+		if (max->data < temp->data)
+			max = temp;
+		temp = temp->next;
+	}
+	return reverse(head);
+}
+
